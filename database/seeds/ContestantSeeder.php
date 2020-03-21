5ac992871d6ff0f6233ac69216d2ea294adb38df
Andikan Gabriel <andikanonline@gmail.com>
2020-03-21 14:29:50 +0100

    Add contestant seeder

--- database/seeds/ContestantSeeder.php
+++ database/seeds/ContestantSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ContestantSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(ContestantSeeder::class);
+    	factory(App\Contestant::class, 4)->create();
     }
 }
