951f0a3a4154229298bcbc2c99828748be09d589
Andikan Gabriel <andikanonline@gmail.com>
2020-03-21 14:30:16 +0100

    Add contestant model

---
+++ app/Contestant.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Illuminate\Database\Eloquent\Model;
+
+class Contestant extends Model
+{
+	use HasSlug;
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function votes()
+    {
+        return $this->hasMany(Votes::class);
+    }
+}
