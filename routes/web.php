e0818a8f52f1f744a1b1c8160cb61cff327b2225
Andikan Gabriel <andikanonline@gmail.com>
2020-03-21 18:38:12 +0100

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,12 +17,65 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{home?}', function () {
     $contestants = \App\Contestant::all();
 
-    // return view('welcome')->withContestants($contestants);
+    return view('welcome')->withContestants($contestants);
 });
 
 Route::get('/vote/{contestant_slug}', function ($contestant_slug) {
 
-    // $contestant = \App\Contestant::whereSlug($contestant_slug)->first();
-// dd($contestant->votes);
-    // return view('vote')->withContestant($contestant);
+    $contestant = \App\Contestant::whereSlug($contestant_slug)->first();
+
+	$votes 		= $contestant->votesCount->votes;
+
+    return view('vote', [
+    	'contestant' => $contestant,
+    	'votes'		 => $votes,
+    ]);
+});
+
+Route::post('/vote/verifypayment/{contestant_slug}', function ($contestant_slug, Request $request) {
+	$ref = $request->input('reference');
+
+	// Validate posted request
+	if (empty($ref) || !is_string($ref)) {
+		return response()->json([
+			'status' => true
+		], 200);
+	} else {
+		$url = 'https://api.paystack.co/transaction/verify/'.$ref;
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt(
+		  $ch, CURLOPT_HTTPHEADER, [
+		    'Authorization: Bearer sk_test_9127302b2a2077a7418e3ee7f8a99a540f5cde48']
+		);
+
+		//send request
+		$request = curl_exec($ch);
+
+		//close connection
+		curl_close($ch);
+
+		//declare an array that will contain the result 
+		$result = array();
+
+		if ($request) {
+		  $result = json_decode($request, true);
+		}
+
+		if (array_key_exists('data', $result) && array_key_exists('status', $result['data']) && ($result['data']['status'] === 'success')) {
+			// Increment the vote
+		    $contestant 	= \App\Contestant::whereSlug($contestant_slug)->first();
+			$votes 			= \App\Votes::whereContestantId($contestant->id)->first();
+
+			$votes->votes 	= $votes->votes+1;
+			
+			$votes->save();
+
+			return true;
+		}else{
+			return false;
+		}
+	}
 });
